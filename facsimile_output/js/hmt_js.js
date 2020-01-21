e03a7503a161f68e03aaaca72baafb628d19e409
Eumaeus <cwblackwell@gmail.com>
2020-01-21 00:10:02 -0500

    Update

--- facsimile_output/js/hmt_js.js
+++ facsimile_output/js/hmt_js.js
@@ -14,57 +14,66 @@ $(document).ready(function () {
 			$(".ohco2_commentBlock_visible").addClass("ohco2_commentBlock_hidden").removeClass("ohco2_commentBlock_visible");
 			$(".cite_showhide_header").next().hide();
 		}
-	})
-
-	var imgOffsetLeft = $("span.cite_preview_image > img.citeImage").first().offset().left;
-	console.log(imgOffsetLeft);
-	var imgOffsetTop = $("span.cite_preview_image > img.citeImage").first().offset().top;
-	console.log(imgOffsetTop);
-	var imgWidth = $("span.cite_preview_image > img.citeImage").first().width();
-	console.log(imgWidth);
-	var imgHeight = $("span.cite_preview_image > img.citeImage").first().height();
-	console.log(imgHeight);
-
-	function getRoiLeft( imgLeft, imgWidth, roiLeft) {
-		var absLeft = imgWidth * roiLeft;
-		return absLeft + imgLeft;		
-	}
-	function getRoiTop( imgTop, imgHeight, roiTop) {
-		var absTop = imgHeight * roiTop;
-		return imgTop + absTop;		
-	}
-	function getRoiWidth( imgWidth, roiWidth) {
-		var absWidth = imgWidth * roiWidth;
-		return absWidth;		
-	}
-	function getRoiHeight( imgHeight, roiHeight) {
-		var absHeight = imgHeight * roiHeight;
-		return absHeight;		
-	}
-	
-	
-
-	console.log(imgOverlayROIs);
-	var temp = new Object();	
-
-	imgOverlayROIs.forEach(roi => {
-		console.log(roi.left);
-		var newDiv = $("<div> </div>");
-		newDiv.addClass("hmt_imageOverlay_roi");
-		newDiv.addClass(roi.class);
-
-		var roi_left = getRoiLeft( imgOffsetLeft, imgWidth, roi.left);
-		var roi_top =  getRoiTop( imgOffsetTop, imgHeight,  roi.top);
-		var roi_width =  getRoiWidth( imgWidth, roi.width);
-		var roi_height =  getRoiHeight( imgHeight, roi.height);
-
-		newDiv.css("width", roi_width );
-		newDiv.css("height", roi_height );
-		newDiv.css("top", roi_top );
-		newDiv.css("left", roi_left );
-
-		$("body").append(newDiv);
 	});
+
+	
+
+	
+		function pollDOM () {
+		  var el = $("span.cite_preview_image > img.citeImage").first();
+		  console.log(el.width());
+
+		  if (el.width() > 100) {
+		  	var imgOffsetLeft = $("span.cite_preview_image > img.citeImage").first().offset().left;
+				var imgOffsetTop = $("span.cite_preview_image > img.citeImage").first().offset().top;
+				var imgWidth = $("span.cite_preview_image > img.citeImage").first().width();
+				var imgHeight = $("span.cite_preview_image > img.citeImage").first().height();
+
+				function getRoiLeft( imgLeft, imgWidth, roiLeft) {
+					var absLeft = imgWidth * roiLeft;
+					return absLeft + imgLeft;		
+				}
+				function getRoiTop( imgTop, imgHeight, roiTop) {
+					var absTop = imgHeight * roiTop;
+					return imgTop + absTop;		
+				}
+				function getRoiWidth( imgWidth, roiWidth) {
+					var absWidth = imgWidth * roiWidth;
+					return absWidth;		
+				}
+				function getRoiHeight( imgHeight, roiHeight) {
+					var absHeight = imgHeight * roiHeight;
+					return absHeight;		
+				}
+
+		    var temp = new Object();	
+
+				imgOverlayROIs.forEach(roi => {
+					var newDiv = $("<div> </div>");
+					newDiv.addClass("hmt_imageOverlay_roi");
+					newDiv.addClass(roi.class);
+
+					var roi_left = getRoiLeft( imgOffsetLeft, imgWidth, roi.left);
+					var roi_top =  getRoiTop( imgOffsetTop, imgHeight,  roi.top);
+					var roi_width =  getRoiWidth( imgWidth, roi.width);
+					var roi_height =  getRoiHeight( imgHeight, roi.height);
+
+					newDiv.css("width", roi_width );
+					newDiv.css("height", roi_height );
+					newDiv.css("top", roi_top );
+					newDiv.css("left", roi_left );
+
+					$("body").append(newDiv);
+				});
+			  
+		  } else {
+		  	console.log("small")
+		    setTimeout(pollDOM, 300); // try again in 300 milliseconds
+		  }
+		}
+	
+		pollDOM();
+	
 
 	$(".cite_preview_image").mouseenter(function(){
 			$(".hmt_imageOverlay_roi").hide();
